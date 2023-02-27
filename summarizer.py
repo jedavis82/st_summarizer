40a28d49455e3d72a90043e176353e674c62c634
jedavis82 <davis.jeremy.e@gmail.com>
2023-02-27 11:55:13 -0600

    Changing layout
    Changed the file input to a label; changed the summary to be computed using "one or two" paragraphs instead of the TL;DR.

--- summarizer.py
+++ summarizer.py
@@ -6,21 +6,22 @@ import openai
 
 class Summarizer: 
     def __init__(self, api_key: str=None): 
-        assert api_key is not None, "Must supply a valid path to an API key file"
+        assert api_key is not None, "Must supply a valid API key"
         # There's no real way to verify the key path is valid without running the model to my knowledge
         openai.api_key = api_key
     
-    def summarize_page(self, url: str=None) -> str: 
+    def summarize_page(self, url: str=None, num_paragraphs: str='one') -> str: 
         """
             Summarize an article from a URL using GPT3
         """
-        text_prompt = f'Summarize the text in the following url: {url}\n\nTl;dr'
+        text_prompt = f'Generate a {num_paragraphs} paragraph summary for the following news article: {url}'
+        # text_prompt = f'Summarize the text in the following url: {url}\n\nTl;dr'
 
         response = openai.Completion.create(
             model="text-davinci-003",
             prompt=text_prompt,
             temperature=0.7,
-            max_tokens=60,
+            max_tokens=1000,
             top_p=1.0,
             frequency_penalty=0.0,
             presence_penalty=1
--- summarizer_app.py
+++ summarizer_app.py
@@ -1,5 +1,5 @@
 import streamlit as st 
-from io import StringIO 
+# from io import StringIO 
 import validators 
 
 from summarizer import Summarizer
@@ -13,8 +13,8 @@ def valid_url(url: str) -> bool:
     else:
         return False
     
-def summarize_article(url: str, model: Summarizer) -> str:
-    summary = model.summarize_page(url)
+def summarize_article(url: str, num_paragraphs: str, model: Summarizer) -> str:
+    summary = model.summarize_page(url, num_paragraphs)
     return summary 
 
 def main():
@@ -24,18 +24,23 @@ def main():
     st.markdown('An API Key can be generated [here](https://platform.openai.com/account/api-keys)')
     st.markdown('Your API key file should contain the generated key in plain text.')
     st.info("Your API key will not be saved. This is required to enable the use of the OpenAI API.")
-    api_key_file = st.file_uploader('Upload your OpenAI API key file')
-    if api_key_file is not None: 
-        file_bytes = StringIO(api_key_file.getvalue().decode('utf-8'))
-        api_key = file_bytes.read()
+    api_key = st.text_input('Paste your OpenAI API Key')
+    # api_key_file = st.file_uploader('Upload your OpenAI API key file')
+    if api_key != '': 
+    # if api_key_file is not None: 
+        # file_bytes = StringIO(api_key_file.getvalue().decode('utf-8'))
+        # api_key = file_bytes.read()
         model = Summarizer(api_key=api_key)
 
         st.subheader('Compute Article Summary')
-        url_input = st.text_input('Enter URL of the form: https://www.google.com/')
-        if url_input: 
+        
+        url_input = st.text_input(label='Enter URL of the form: https://www.google.com/')
+        num_paragraphs_radio = st.radio('Number of paragraphs for the summary', ('One', 'Two'))
+        if url_input != '': 
             if valid_url(url_input):
                 with st.spinner('Processing article...'):
-                    summary = summarize_article(url_input, model)
+                    num_paragraphs = num_paragraphs_radio.lower()
+                    summary = summarize_article(url_input, num_paragraphs, model)
                     st.markdown('### Summary')
                     st.write(summary)
             else: 
