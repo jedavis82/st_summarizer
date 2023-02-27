3ace53f590d6e7c54a18a28e5122b52c3d03cb0f
jedavis82 <davis.jeremy.e@gmail.com>
2023-02-27 16:19:46 -0600

    Updating layout. Adding ability to summarize plain text.

--- summarizer.py
+++ summarizer.py
@@ -5,12 +5,17 @@ Load up the OpenAI API key and pass a document through the API for summarization
 import openai 
 
 class Summarizer: 
-    def __init__(self, api_key: str=None): 
-        assert api_key is not None, "Must supply a valid API key"
-        # There's no real way to verify the key path is valid without running the model to my knowledge
-        openai.api_key = api_key
+    # def __init__(self, api_key: str=None): 
+    #     assert api_key is not None, "Must supply a valid API key"
+    #     # There's no real way to verify the key path is valid without running the model to my knowledge
+    #     openai.api_key = api_key
+
+    def __init__(self, key_file: str='./config/openai.key'):
+        with open(key_file, 'r', encoding='utf-8') as f: 
+            api_key = f.read()
+            openai.api_key = api_key 
     
-    def summarize_page(self, url: str=None, num_paragraphs: str='one') -> str: 
+    def summarize_from_url(self, url: str=None, num_paragraphs: str='one') -> str: 
         """
             Summarize an article from a URL using GPT3
         """
@@ -29,4 +34,23 @@ class Summarizer:
 
         summary = response['choices'][0]['text'].replace(':', '', 1).strip()
         return summary
+    
+    def summarize_from_text(self, text: str=None, num_paragraphs: str='one') -> str: 
+        """
+            Summarize a block of text input using GPT3
+        """
+        text_prompt = f'Generate a {num_paragraphs} paragraph summary for the following text: {text}'
+
+        response = openai.Completion.create(
+            model='text-davinci-003',
+            prompt=text_prompt,
+            temperature=0.7, 
+            max_tokens=1000, 
+            top_p=1.0,
+            frequency_penalty=0.0,
+            presence_penalty=1
+        )
+
+        summary = response['choices'][0]['text'].replace(':', '', 1).strip()
+        return summary 
 
--- summarizer_app.py
+++ summarizer_app.py
@@ -13,38 +13,70 @@ def valid_url(url: str) -> bool:
     else:
         return False
     
-def summarize_article(url: str, num_paragraphs: str, model: Summarizer) -> str:
-    summary = model.summarize_page(url, num_paragraphs)
+def summarize_news_article(url: str, num_paragraphs: str, model: Summarizer) -> str:
+    summary = model.summarize_from_url(url, num_paragraphs)
+    return summary 
+
+def summarize_plain_text(text: str, num_paragraphs: str, model: Summarizer) -> str: 
+    summary = model.summarize_from_text(text, num_paragraphs)
     return summary 
 
 def main():
-    st.title('Short Summary of News Articles')
+    ## OLD VERSION BEFORE I HAD THE KEY EMBEDDED 
+    # st.title('Short Summary of News Articles')
 
-    st.header('OpenAI API Key')
-    st.markdown('An API Key can be generated [here](https://platform.openai.com/account/api-keys)')
-    st.markdown('Your API key file should contain the generated key in plain text.')
-    st.info("Your API key will not be saved. This is required to enable the use of the OpenAI API.")
-    api_key = st.text_input('Paste your OpenAI API Key')
-    # api_key_file = st.file_uploader('Upload your OpenAI API key file')
-    if api_key != '': 
-    # if api_key_file is not None: 
-        # file_bytes = StringIO(api_key_file.getvalue().decode('utf-8'))
-        # api_key = file_bytes.read()
-        model = Summarizer(api_key=api_key)
+    # st.header('OpenAI API Key')
+    # st.markdown('An API Key can be generated [here](https://platform.openai.com/account/api-keys)')
+    # st.markdown('Your API key file should contain the generated key in plain text.')
+    # st.info("Your API key will not be saved. This is required to enable the use of the OpenAI API.")
+    # api_key = st.text_input('Paste your OpenAI API Key')
+    # # api_key_file = st.file_uploader('Upload your OpenAI API key file')
+    # if api_key != '': 
+    # # if api_key_file is not None: 
+    #     # file_bytes = StringIO(api_key_file.getvalue().decode('utf-8'))
+    #     # api_key = file_bytes.read()
+    #     model = Summarizer(api_key=api_key)
 
-        st.subheader('Compute Article Summary')
+    #     st.subheader('Compute Article Summary')
         
-        url_input = st.text_input(label='Enter URL of the form: https://www.google.com/')
-        num_paragraphs_radio = st.radio('Number of paragraphs for the summary', ('One', 'Two'))
+    #     url_input = st.text_input(label='Enter URL of the form: https://www.google.com/')
+    #     num_paragraphs_radio = st.radio('Number of paragraphs for the summary', ('One', 'Two'))
+    #     if url_input != '': 
+    #         if valid_url(url_input):
+    #             with st.spinner('Processing article...'):
+    #                 num_paragraphs = num_paragraphs_radio.lower()
+    #                 summary = summarize_article(url_input, num_paragraphs, model)
+    #                 st.markdown('### Summary')
+    #                 st.write(summary)
+    #         else: 
+    #             st.error("Please supply a valid URL")
+
+    model = Summarizer()
+    with st.sidebar: 
+        choose = st.selectbox('Choose a type of document to summarize', ("News Article", "Plain Text"))
+    
+    num_paragraphs_radio = st.radio('Number of paragraphs for the summary', ('One', 'Two'))
+    if choose == 'News Article':
+        st.title('Short Summary of a News Article From a Given URL') 
+        url_input = st.text_input(label='Enter URL of the form: https://www.google.com')
         if url_input != '': 
-            if valid_url(url_input):
-                with st.spinner('Processing article...'):
+            if valid_url(url_input): 
+                with st.spinner('Processing article...'): 
                     num_paragraphs = num_paragraphs_radio.lower()
-                    summary = summarize_article(url_input, num_paragraphs, model)
+                    summary = summarize_news_article(url_input, num_paragraphs, model)
                     st.markdown('### Summary')
                     st.write(summary)
             else: 
-                st.error("Please supply a valid URL")
+                st.error('Please supply a valid URL')
+    elif choose == 'Plain Text': 
+        st.title('Short Summary of a Text Snippet')
+        text_to_summ = st.text_area(label='Enter the text you want to summarize')
+        if text_to_summ != '': 
+            with st.spinner('Processing text...'): 
+                num_paragraphs = num_paragraphs_radio.lower()
+                summary = summarize_plain_text(text_to_summ, num_paragraphs, model)
+                st.markdown('### Summary')
+                st.write(summary)
 
 if __name__ == '__main__':
     main()
